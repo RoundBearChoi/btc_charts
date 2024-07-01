665bf392e519309f41c2f41cea975a9fb2ec2826
RoundBearChoi <roundbeargames@gmail.com>
2024-07-01 20:36:40 +0900

    save

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import logging
 import pandas
+import numpy
 import os
 import math
+import dateutil
 
 from datetime import date, timedelta
+from dateutil.relativedelta import relativedelta
+from matplotlib.colors import LinearSegmentedColormap
 
 
 def download_daily_btc_data(total_years):
@@ -66,7 +70,6 @@ def draw_pi_top(data_frame, block_window):
     plt.ylabel('Price (USD)')
     plt.legend()
 
-    print('')
     print('Drawing Pi Top..')
 
     plt.show(block=block_window)
@@ -90,12 +93,11 @@ def draw_pi_bottom(data_frame, block_window):
 
     axis = plt.gca()
     axis.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, p: format(int(x), ',')))
-    
+
     plt.title('Pi Bottom')
     plt.ylabel('Price (USD)')
     plt.legend(['BTC Price', '471 MA * 0.475', '150 EMA * 0.475'])
 
-    print('')
     print('Drawing Pi Bottom..')
 
     plt.show(block=block_window)
@@ -111,16 +113,64 @@ def draw_weeks_moving_average(data_frame, weeks, block_window):
 
     plt.plot(data_frame['close'], label='Bitcoin Price', linewidth=1)
     plt.plot(data_frame['moving_avg'], label=f'{weeks}-Week Moving Average', linewidth=1)
-    plt.title('Moving Average')
 
     axis = plt.gca()
     axis.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, p: format(int(x), ',')))
 
+    plt.title('Moving Average')
     plt.ylabel('Price (USD)')
     plt.legend()
 
-    print('')
     print('Drawing Weeks Moving Average..')
+
+    plt.show(block=block_window)
+
+
+def draw_rsi_vs_halving(data_frame, block_window):
+    plt.figure(figsize=(12, 6))  # A new window
+
+    plt.style.use('fast')
+    plt.grid(False)
+
+    data_frame = data_frame.resample('ME').last().asfreq('ME')
+
+    # Calculate the RSI
+    delta = data_frame['close'].diff()
+    up = delta.clip(lower=0)
+    down = -1*delta.clip(upper=0)
+    ema_up = up.ewm(com=13, adjust=False).mean()
+    ema_down = down.ewm(com=13, adjust=False).mean()
+    rs = ema_up/ema_down
+
+    data_frame['RSI'] = 100 - (100/(1 + rs))
+
+    # 2028-04-17 is next predicted date
+    halving_dates = ['2012-11-28', '2016-07-09', '2020-05-11', '2024-04-19', '2028-04-17']
+    halving_dates = pandas.to_datetime(halving_dates)
+    halving_dates = pandas.DatetimeIndex(halving_dates)
+
+    norm = plt.Normalize(0, 50)
+
+    for i in numpy.arange(1, len(data_frame)):
+        x_values = data_frame.index[i - 1:i + 1]
+        y_values = data_frame['RSI'][i - 1:i + 1]
+
+        months_left = None
+
+        for halving_date in halving_dates:
+            if x_values[1] < halving_date:
+                diff = relativedelta(halving_date, x_values[0])
+                months_left = diff.years * 12 + diff.months
+                break
+
+        cmap = LinearSegmentedColormap.from_list('my_cmap', ['lightgreen', 'red'])
+        color_value = cmap(norm(months_left))
+
+        plt.plot(x_values, y_values, color=color_value)
+
+    plt.title('Monthly RSI vs Next Halving')
+
+    print('Drawing Monthly RSI vs Next Halving..')
 
     plt.show(block=block_window)
 
@@ -141,9 +191,11 @@ def run():
     print('Log file saved at: ' + os.path.abspath(log_file_name))
 
     # Draw graphs
+    print('')
     draw_pi_top(daily, False)
     draw_pi_bottom(daily, False)
-    draw_weeks_moving_average(daily, 140, True)
+    draw_weeks_moving_average(daily, 140, False)
+    draw_rsi_vs_halving(daily, True)
 
 
 if __name__ == '__main__':
