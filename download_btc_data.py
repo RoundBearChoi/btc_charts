3d80048db4c124cd5f468847baabe27b462ddd79
RoundBearChoi <roundbeargames@gmail.com>
2024-07-01 22:08:07 +0900

    save

---
+++ download_btc_data.py
@@ -0,0 +1,40 @@
+import cryptocompare
+import pandas
+import math
+
+from datetime import date, timedelta
+
+
+def download_daily_btc_data(total_years):
+    total_days = math.ceil(total_years * 365)
+    today = date.today()
+    num_downloads = total_days // 2000
+    days_per_download = 2000
+    remaining_days = total_days % 2000
+
+    print('Downloading ' + str(total_days) + ' days worth of data from cryptocompare..')
+    print('(' + str(total_years) + ' years)')
+
+    data = []
+
+    for i in range(num_downloads):
+        download_date = today - timedelta(days=i * days_per_download)
+        data_part = cryptocompare.get_historical_price_day(
+            'BTC', currency='USD', limit=2000, toTs=download_date)
+        data += data_part
+
+    # Download remaining days
+    if remaining_days > 0:
+        download_date = today - timedelta(days=num_downloads * days_per_download)
+        data_part = cryptocompare.get_historical_price_day(
+            'BTC', currency='USD', limit=remaining_days, toTs=download_date)
+        data += data_part
+
+    df = pandas.DataFrame(data)
+    df['time'] = pandas.to_datetime(df['time'], unit='s')
+    df.set_index('time', inplace=True, drop=False)
+
+    # Resample to daily
+    df = df.resample('D').last().asfreq('D')
+
+    return df
--- main.py
+++ main.py
@@ -1,50 +1,12 @@
-import cryptocompare
 import logging
-import pandas
 import os
-import math
 
-from datetime import date, timedelta
+from download_btc_data import download_daily_btc_data
 from pi_top import draw_pi_top
 from pi_bottom import draw_pi_bottom
 from moving_average_weeks import draw_moving_average_weeks
 from ema_vs_sma import draw_21ema_vs_50sma
 from rsi_vs_halving import draw_rsi_vs_halving
-
-
-def download_daily_btc_data(total_years):
-    total_days = math.ceil(total_years * 365)
-    today = date.today()
-    num_downloads = total_days // 2000
-    days_per_download = 2000
-    remaining_days = total_days % 2000
-
-    print('Downloading ' + str(total_days) + ' days worth of data from cryptocompare..')
-    print('(' + str(total_years) + ' years)')
-
-    data = []
-
-    for i in range(num_downloads):
-        download_date = today - timedelta(days=i * days_per_download)
-        data_part = cryptocompare.get_historical_price_day(
-            'BTC', currency='USD', limit=2000, toTs=download_date)
-        data += data_part
-
-    # Download remaining days
-    if remaining_days > 0:
-        download_date = today - timedelta(days=num_downloads * days_per_download)
-        data_part = cryptocompare.get_historical_price_day(
-            'BTC', currency='USD', limit=remaining_days, toTs=download_date)
-        data += data_part
-
-    df = pandas.DataFrame(data)
-    df['time'] = pandas.to_datetime(df['time'], unit='s')
-    df.set_index('time', inplace=True, drop=False)
-
-    # Resample to daily
-    df = df.resample('D').last().asfreq('D')
-
-    return df
 
 
 def run():
