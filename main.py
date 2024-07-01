a470986938a2b06f77a5f1bb2d25ac064654970b
RoundBearChoi <roundbeargames@gmail.com>
2024-07-01 22:05:11 +0900

    save

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import cryptocompare
 import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker
 import logging
 import pandas
 import numpy
@@ -13,6 +12,7 @@ from matplotlib.colors import LinearSegmentedColormap
 from pi_top import draw_pi_top
 from pi_bottom import draw_pi_bottom
 from moving_average_weeks import draw_moving_average_weeks
+from ema_vs_sma import draw_21ema_vs_50sma
 
 
 def download_daily_btc_data(total_years):
@@ -48,33 +48,6 @@ def download_daily_btc_data(total_years):
     df = df.resample('D').last().asfreq('D')
 
     return df
-
-
-def draw_21ema_vs_50sma(data_frame, block_window):
-    plt.figure(figsize=(12, 6))  # A new window
-
-    # Calculate EMA21 and SMA50
-    data_frame['EMA21'] = data_frame['close'].ewm(span=21, adjust=False).mean()
-    data_frame['SMA50'] = data_frame['close'].rolling(window=50).mean()
-
-    plt.style.use('fast')
-    plt.grid(False)
-
-    # Plot closing price, EMA21 and SMA50
-    plt.plot(data_frame.index, data_frame['close'], label='Bitcoin Close Price', linewidth=0.55)
-    plt.plot(data_frame.index, data_frame['EMA21'], label='21 Day EMA', linewidth=0.85)
-    plt.plot(data_frame.index, data_frame['SMA50'], label='50 Day SMA', linewidth=0.85)
-
-    plt.title('21-Day Exponential Moving Average vs 50-Day Simple Moving Average')
-    plt.ylabel('Price (USD)')
-    plt.legend()
-
-    axis = plt.gca()
-    axis.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, p: format(int(x), ',')))
-
-    print('Drawing 21EMA vs 50SMA..')
-
-    plt.show(block=block_window)
 
 
 def draw_rsi_vs_halving(data_frame, block_window):
