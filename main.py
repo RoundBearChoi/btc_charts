aff2f5af099d3f16222048871c4b34cff1f6c505
RoundBearChoi <roundbeargames@gmail.com>
2024-07-01 22:01:21 +0900

    save

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import math
 from datetime import date, timedelta
 from dateutil.relativedelta import relativedelta
 from matplotlib.colors import LinearSegmentedColormap
+from pi_top import draw_pi_top
 
 
 def download_daily_btc_data(total_years):
@@ -45,33 +46,6 @@ def download_daily_btc_data(total_years):
     df = df.resample('D').last().asfreq('D')
 
     return df
-
-
-def draw_pi_top(data_frame, block_window):
-    plt.figure(figsize=(12, 6))  # A new window
-
-    plt.style.use('fast')
-    plt.grid(False)
-
-    slow = 365
-    quick = math.floor(slow / 3.14159)
-    data_frame[str(slow) + '_MA * 2'] = data_frame['close'].rolling(window=slow).mean() * 2
-    data_frame[str(quick) + '_MA'] = data_frame['close'].rolling(window=quick).mean()
-
-    plt.plot(data_frame['close'], label='BTC Price', linewidth=0.4)
-    plt.plot(data_frame[str(slow) + '_MA * 2'], label=str(slow) + '-day MA * 2', linewidth=1)
-    plt.plot(data_frame[str(quick) + '_MA'], label=str(quick) + '-day MA', linewidth=1)
-
-    axis = plt.gca()
-    axis.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, p: format(int(x), ',')))
-
-    plt.title('Pi Top')
-    plt.ylabel('Price (USD)')
-    plt.legend()
-
-    print('Drawing Pi Top..')
-
-    plt.show(block=block_window)
 
 
 def draw_pi_bottom(data_frame, block_window):
---
+++ pi_top.py
@@ -0,0 +1,31 @@
+
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import math
+
+
+def draw_pi_top(data_frame, block_window):
+    plt.figure(figsize=(12, 6))  # A new window
+
+    plt.style.use('fast')
+    plt.grid(False)
+
+    slow = 365
+    quick = math.floor(slow / 3.14159)
+    data_frame[str(slow) + '_MA * 2'] = data_frame['close'].rolling(window=slow).mean() * 2
+    data_frame[str(quick) + '_MA'] = data_frame['close'].rolling(window=quick).mean()
+
+    plt.plot(data_frame['close'], label='BTC Price', linewidth=0.4)
+    plt.plot(data_frame[str(slow) + '_MA * 2'], label=str(slow) + '-day MA * 2', linewidth=1)
+    plt.plot(data_frame[str(quick) + '_MA'], label=str(quick) + '-day MA', linewidth=1)
+
+    axis = plt.gca()
+    axis.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, p: format(int(x), ',')))
+
+    plt.title('Pi Top')
+    plt.ylabel('Price (USD)')
+    plt.legend()
+
+    print('Drawing Pi Top..')
+
+    plt.show(block=block_window)
