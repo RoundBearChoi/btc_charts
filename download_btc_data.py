61731d616e328bec30233deff5d6e76e8b8904d3
RoundBearChoi <roundbeargames@gmail.com>
2024-07-01 22:30:33 +0900

    save

--- download_btc_data.py
+++ download_btc_data.py
@@ -1,13 +1,12 @@
 import cryptocompare
 import pandas
 import math
-
-from datetime import date, timedelta
+import datetime as dt
 
 
 def download_daily_btc_data(total_years):
     total_days = math.ceil(total_years * 365)
-    today = date.today()
+    today = dt.date.today()
     num_downloads = total_days // 2000
     days_per_download = 2000
     remaining_days = total_days % 2000
@@ -19,14 +18,14 @@ def download_daily_btc_data(total_years):
     data = []
 
     for i in range(num_downloads):
-        download_date = today - timedelta(days=i * days_per_download)
+        download_date = today - dt.timedelta(days=i * days_per_download)
         data_part = cryptocompare.get_historical_price_day(
             'BTC', currency='USD', limit=2000, toTs=download_date)
         data += data_part
 
     # Download remaining days
     if remaining_days > 0:
-        download_date = today - timedelta(days=num_downloads * days_per_download)
+        download_date = today - dt.timedelta(days=num_downloads * days_per_download)
         data_part = cryptocompare.get_historical_price_day(
             'BTC', currency='USD', limit=remaining_days, toTs=download_date)
         data += data_part
